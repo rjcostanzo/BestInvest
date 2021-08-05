15c2567a70497cc22a8ace1c94d9a95c25301394
SteveHoltRules <matt.c.luna@gmail.com>
2021-08-04 20:58:57 -0500

    Add API 2

--- assets/script.js
+++ assets/script.js
@@ -28,6 +28,31 @@ var getQuote = function (ticker) {
     });
 };
 
+var getCall = function (call) {
+  //Sample ticker for proof of concept
+  // var ticker = `amzn`;
+  var apiUrl = `https://www.quandl.com/api/v3/datasets/${call}?start_date=2021-07-29&end_date=2021-07-29&api_key=3sfizins6VRG4tN1qVpz`;
+  // var apiUrl = `https://www.quandl.com/api/v3/datasets/CHRIS/ASX_WM1?start_date=2021-06-29&end_date=2021-06-29&api_key=3sfizins6VRG4tN1qVpz';
+  console.log(apiUrl);
+  fetch(apiUrl).then(function (response) {
+    //request was successful
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data);
+        console.log(data[0].bidPrice);
+        //do something here....
+      });
+    } else {
+      alert("Error: Quandl quote is not found");
+    }
+  })
+    .catch(function (error) {
+      //notice this catch getting changed out to the end of the .then()
+      alert("Unable to connect to Quandl");
+    });
+};
+
+
 //Create a modal to prompt the user to input a stock ticker. Give recommendations
 
 
@@ -41,6 +66,6 @@ var getQuote = function (ticker) {
 
 //Create a model to display the stock in terms of the raw material
 
-
+getCall('CHRIS/CME_461');
 
 getQuote('amzn');
